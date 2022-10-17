7906f20c8adacc52fa247db06fbc903918d28764
ANIKETH SATYANARAYANA <46114763+anikeths@users.noreply.github.com>
2022-10-16 23:34:41 -0700

    Update consumer.py

--- Codelab 4/consumer/code/consumer.py
+++ Codelab 4/consumer/code/consumer.py
@@ -14,11 +14,12 @@ def randres():
 #communicates with the API container and writes what it is getting from api TO food.html template file and renders it.
 @app.route('/',methods= ['GET'])
 def index():
-    res = requests.get('http://api:5000/api/random')
+    res= requests.get('http://' + str(os.environ.get("API_HOST")) + ':' + str(os.environ.get("API_PORT")) + "/" + str(os.environ.get("API_ENDPOINT")))
+#     res = requests.get('http://api:5000/api/random')
     food_item = res.text
     return render_template('food.html', food=food_item)
 
 if __name__ == "__main__":
-    # port = int(os.environ.get('PORT', 3001))
-    # app.run('127.0.0.1', port = port,debug=True)
     app.run(debug=True, host='0.0.0.0', port=80)
+    # port = os.environ.get('CONSUMER_PORT')
+    # app.run('0.0.0.0', port = port,debug=True)
