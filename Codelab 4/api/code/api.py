bcab076da2d5ca6fb96c1b9eb767267c173e4b04
ANIKETH SATYANARAYANA <46114763+anikeths@users.noreply.github.com>
2022-10-16 23:32:15 -0700

    Update api.py

--- Codelab 4/api/code/api.py
+++ Codelab 4/api/code/api.py
@@ -20,12 +20,12 @@ def food_dictionary():
 #displays the random food pick on localhost:5000/api/random/
 @app.route('/api/random',methods= ['GET'])
 def randres():
-    #  res = key,val = random.choice(list(food.items()))
-     #  return (str(res))
-      #    return dictionary[choice(list(dictionary.keys()))]
        key1 = random.choice(list(food))
        return str(key1) + ':' + str(food[key1])
 
 if __name__ == "__main__":
      app.run('0.0.0.0', port=5000,debug=True)
+     #to avoid hard-coded dependency
+#      port= os.environ.get('API_PORT')
+#      app.run('0.0.0.0', port=port,debug=True)
     
