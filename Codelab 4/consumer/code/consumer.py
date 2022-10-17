ff06080da1da55ada24f75c5a6ccdd2ec2e29781
ANIKETH SATYANARAYANA <46114763+anikeths@users.noreply.github.com>
2022-10-16 23:48:58 -0700

    Update consumer.py

--- Codelab 4/consumer/code/consumer.py
+++ Codelab 4/consumer/code/consumer.py
@@ -20,6 +20,6 @@ def index():
     return render_template('food.html', food=food_item)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
-    # port = os.environ.get('CONSUMER_PORT')
-    # app.run('0.0.0.0', port = port,debug=True)
+#     app.run(debug=True, host='0.0.0.0', port=80)
+    port = os.environ.get('CONSUMER_PORT')
+    app.run('0.0.0.0', port = port,debug=True)
