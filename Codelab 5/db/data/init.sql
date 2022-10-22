a83a8d5e040a9bc6ca32c522b10e3f7624d1815e
ANIKETH SATYANARAYANA <46114763+anikeths@users.noreply.github.com>
2022-10-21 17:13:01 -0700

    Create init.sql

---
+++ Codelab 5/db/data/init.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS WEEK_5;
+CREATE DATABASE WEEK_5;
+
+DROP TABLE IF EXISTS food;
+CREATE TABLE food(foodname VARCHAR(255), price INT);
+INSERT INTO food(foodname, price) VALUES('idli', 522);
+INSERT INTO food(foodname, price) VALUES('dosa', 20);
+INSERT INTO food(foodname, price) VALUES('poha', 14);
+INSERT INTO food(foodname, price) VALUES('puliyogare', 522);
+INSERT INTO food(foodname, price) VALUES('murmura', 20);
+INSERT INTO food(foodname, price) VALUES('paddu', 14);
+INSERT INTO food(foodname, price) VALUES('gobi', 522);
+INSERT INTO food(foodname, price) VALUES('pesarattu', 20);
+INSERT INTO food(foodname, price) VALUES('kottu paratha', 14);
+INSERT INTO food(foodname, price) VALUES('upma', 522);
+INSERT INTO food(foodname, price) VALUES('sambar vada', 20);
+INSERT INTO food(foodname, price) VALUES('popcorn', 14);
+INSERT INTO food(foodname, price) VALUES('fudge brownie', 522);
+INSERT INTO food(foodname, price) VALUES('roti', 20);
+INSERT INTO food(foodname, price) VALUES('banana split', 14);
